e5d66e363710a7a91919f95698c6406e821b29a8
Khanh Du <luongkhanhdu95@gmail.com>
2024-12-06 14:36:30 +0700

    Home page and left side bar for PC and mobile

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,32 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+
+  const loggedIn = {firstName: 'You'}
+
   return (
-    <div>Home</div>
+    <>
+    <section className='home'>
+        <div className="home-content">
+            <header className='home-header'>
+                <HeaderBox 
+                  type="greeting"
+                  title="Welcome"
+                  user={loggedIn?.firstName || 'Guest'}
+                  subtext="Just a subtext"
+                />
+
+                <TotalBalanceBox 
+                  accounts={[]}
+                  totalBanks={1}
+                  totalCurrentBalance={1250.35}
+                />
+            </header>
+        </div>
+    </section>
+    </>
   )
 }
 
