3f6561de925a831a09240801d86c693016451bdc
Khanh Du <luongkhanhdu95@gmail.com>
2024-12-09 13:08:16 +0700

    route for pages and add shadcn form with custom input and basic validation

---
+++ components/CustomInput.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage } from './ui/form'
+import { Input } from './ui/input'
+import { Control, FieldValues, Form, Path, UseFormReturn } from 'react-hook-form'
+import { z } from 'zod'
+
+interface CustomInputProps<T extends FieldValues>{
+    form: UseFormReturn<T>;
+    name: Path<T>;
+    label: string;
+    placeholder?: string;
+}
+
+const CustomInput = <T extends FieldValues>({
+    form,
+    name,
+    label,
+    placeholder,
+}: CustomInputProps<T>) => {
+    return (
+        <>
+            <FormField
+                control={form.control}
+                name={name}
+                render={({ field }) => (
+                    <FormItem className="form-item">
+                        <FormLabel className="form-label">{label}</FormLabel>
+                        <div className="flex w-full flex-col">
+                            <FormControl>
+                                <Input
+                                    placeholder={placeholder}
+                                    className="input-class"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription></FormDescription>
+                            <FormMessage className="form-message mt-2" />
+                        </div>
+                    </FormItem>
+                )}
+            />
+        </>
+    )
+}
+
+export default CustomInput
